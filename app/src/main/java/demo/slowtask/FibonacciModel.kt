58da5d31992f996a9463aef8b76426fdd976a166
Cristian Esquivias <cesquivias@varomoney.com>
2018-09-17 13:25:37 -0700

    Do sequence in separate thread. Use longs

--- app/src/main/java/demo/slowtask/FibonacciModel.kt
+++ app/src/main/java/demo/slowtask/FibonacciModel.kt
@@ -1,35 +1,38 @@
 package demo.slowtask
 
 /**
- * Returns the fibonacci sequence very slowly to the callback. Initally, the sequence is stopped.
+ * Returns the fibonacci sequence very slowly to the callback. Initially, the sequence is stopped.
  * To resume or stop the sequence set enabled to true & false, respectively.
  */
 class FibonacciModel {
     /**
      * When a new number in the sequence is computed, it will be called to this callback
      */
-    var callback: ((Int) -> Unit)? = null
+    var callback: ((Long) -> Unit)? = null
 
     /**
      * The sequence is started when enabled is true and stops when it's set to false
      */
     var enabled = false
         set(value) {
+            field = value
             if (value) {
                 start()
             }
         }
 
-    private var a = 1
-    private var b = 1
+    private var a = 1L
+    private var b = 1L
 
     private fun start() {
-        while (enabled) {
-            callback?.invoke(a)
-            val temp = b
-            b += a
-            a = temp
-            Thread.sleep(2000)
-        }
+        Thread {
+            while (enabled) {
+                callback?.invoke(a)
+                val temp = b
+                b += a
+                a = temp
+                Thread.sleep(2000)
+            }
+        }.start()
     }
 }
